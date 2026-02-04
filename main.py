110d331b37a1cd5f68fdfde3201f6e3c36737237
Avi Gross <avigross2018@gmail.com>
2026-02-04 16:26:56 +0200

    final-step2-menu

--- main.py
+++ main.py
@@ -1,29 +1,44 @@
-# from checking_functions import manager_authentication
+from checking_functions import manager_authentication, file_exist
+from pathlib import Path
+from actions_on_files import check_csv
 
 
+
+budget_file = Path('./budget.txt')
+airport_entry_file = Path('./airport_entry_fee.csv')
+continents_pricing_file = Path('./continents_pricing.csv')
+credentials_file = Path('./credential.csv')
 
 def menu():
+    bud_check = file_exist(budget_file)
+    cred_check = file_exist(credentials_file)
+    if bud_check == False or cred_check == False:
+        return "One of the files or both doesn't exist"
     flag = False
-    user=input('please choise, to manager press: 1: to passenger press: 2:')
-    while flag== False:
-         try:
-            if int(user) == 1:
-                manager_name=input('please enter your name: ')
-                manager_pw=input('please enter your password: ')
-                # manager_authentication(manager_name, manager_pw, file_path)
-                flag = True
-            elif int(user) == 2:
-                flag=True
-                pass
-            else:
-                user = input('please choise only 1 or 2, to manager press: 1 to passenger press: 2')
-         except:
-            print('invaid input')
-            user = input('please choise, to manager press: 1 to passenger press: 2')
+    while flag == False:
+        user = input('please choise, to manager press: 1: to passenger press: 2:')
+        if user == '1':
+            manager_name = input('please enter your name: ')
+            manager_pw = input('please enter your password: ')
+            cred_list = check_csv(credentials_file)
+            check_manager_info = manager_authentication(manager_name, manager_pw, cred_list)
+            if check_manager_info == False:
+                return 'Incorrect user name, or password'
+            
+            flag = True
+        elif user == '2':
+            flag = True
+            pass
+        else:
+            print('Invalid input')
+            continue
+
+                
 
 
 
 
 
 
-menu()
+
+print(menu())
