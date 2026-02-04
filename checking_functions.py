756d966629df2180ba87d42144ed8462181256d7
Avi Gross <avigross2018@gmail.com>
2026-02-04 15:12:21 +0200

    func-aouth-ready

--- checking_functions.py
+++ checking_functions.py
@@ -1,4 +1,6 @@
 from pathlib import Path
+import csv
+from actions_on_files import check_csv
 def file_exist(path):
     '''
     Docstring for file_exist
@@ -9,3 +11,21 @@ def file_exist(path):
         return True
     return False
 
+def manager_authentication(manager_name, manager_pw, file_path):
+    '''
+    Docstring for manager_authentication
+    compare between manager info and credentials.csv
+    :param manager_name: get str name
+    :param manager_pw: get str password
+    :param file_path: get variable that contain check_csv function
+    '''
+    manager_info = [manager_name, manager_pw]
+    flag = False
+    for list in file_path:
+        if manager_info == list:
+            flag = True
+    return flag
+
+
+        
+        
